4caa0401903bb9c755222e7805ec83167edea4a0
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-22 13:26:47 +0000

    QS: add newly added tiles
    * Battery saver
    * Brightness
    * Expanded desktop
    * Screen off
    
    * also add notifications tile
    
    Change-Id: I64956333bd22d26db6ce72a6149b496d4542bfec
    
    Conflicts:
    	res/values/custom_strings.xml
    	src/com/android/settings/SettingsActivity.java
    
    Conflicts:
    	src/com/android/settings/Settings.java

--- src/com/android/settings/desolation/qs/QSTileHolder.java
+++ src/com/android/settings/desolation/qs/QSTileHolder.java
@@ -145,6 +145,22 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;
                 break;
+            case QSConstants.TILE_NOTIFICATIONS:
+                resourceName = "ic_qs_ringer_audible";
+                stringId = R.string.qs_notifications_tile;
+                break;
+            case QSConstants.TILE_BATTERY_SAVER:
+                resourceName = "ic_qs_battery_saver_on";
+                stringId = R.string.qs_battery_saver_tile;
+                break;
+            case QSConstants.TILE_BRIGHTNESS:
+                resourceName = "ic_qs_brightness_auto_off";
+                stringId = R.string.qs_brightness_tile;
+                break;
+            case QSConstants.TILE_SCREEN_OFF:
+                resourceName = "ic_qs_power";
+                stringId = R.string.qs_screen_off_tile;
+                break;
             default:
                 return null;
         }
