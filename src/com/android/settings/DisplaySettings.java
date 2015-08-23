cd9819ad09cdcf9365145047e8c92c7e72a50110
Tyler Dunn <robotdude@att.net>
2015-08-23 00:25:49 -0400

    Revert "Allow control over defaults for Ambient Display in device overlay"
    This reverts commit 404253da6db192f32c38edae9e3b8ccd481a4cec.

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -205,7 +205,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         }
 
         mDozeFragement = (PreferenceScreen) findPreference(KEY_DOZE_FRAGMENT);
-        if (!getResources().getBoolean(com.android.internal.R.bool.config_doze_enabled_by_default)) {
+        if (!isDozeAvailable(activity)) {
             getPreferenceScreen().removePreference(mDozeFragement);
         }
 
@@ -460,7 +460,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (mLiftToWakePreference != null) {
             int value = Settings.Secure.getInt(getContentResolver(), WAKE_GESTURE_ENABLED, 0);
             mLiftToWakePreference.setChecked(value != 0);
-        }        
+        }
     }
 
     private void updateScreenSaverSummary() {
